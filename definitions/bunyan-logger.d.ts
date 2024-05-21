288969e2159ae1b6be0e3e6bd285ab91e553f543
Marius Ursache <marius@tabletcommand.com>
2024-05-21 10:16:34 +1000

    Remove unused definitions

--- definitions/bunyan-logger.d.ts
+++
@@ -1,4 +0,0 @@
-import bunyan from "bunyan";
-export default function logger(name: string, filePath: string, logToConsole: boolean, logToFile: boolean): bunyan | null;
-export declare type LoggerModule = ReturnType<typeof logger>;
-//# sourceMappingURL=bunyan-logger.d.ts.map
